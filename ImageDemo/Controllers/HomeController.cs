f4b1ad35d46a921764c09700517b4f425296a00c
Patrick <Patrick@DESKTOP-E6BBE1C>
2017-05-12 14:43:58 +0200

    Add project files.

---
+++ ImageDemo/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ImageDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase file)
+        {
+
+            if (file != null)
+            {
+                // TODO: Check if file is image
+
+                string pic = System.IO.Path.GetFileName(file.FileName);
+
+                // Virtualise this (VirtualMap)
+                var dir = Server.MapPath("~/Images");
+                var path = System.IO.Path.Combine(dir, pic);
+
+                // Virtualise this part
+                if (!Directory.Exists(dir))
+                {
+                    Directory.CreateDirectory(dir);
+                }
+
+                file.SaveAs(path);
+
+                // Copying the memory for file manipulating i.e. for storing
+                // other places, or sending
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    file.InputStream.CopyTo(ms);
+                    byte[] array = ms.GetBuffer();
+                }
+
+            }
+            // after successfully uploading redirect the user
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
